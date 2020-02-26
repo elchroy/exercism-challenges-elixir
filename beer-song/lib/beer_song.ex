b397ba599de5a1a399ed9e72e19101921334fa9d
elchroy <elchroy@andela.com>
2020-02-26 01:07:00 +0100

    solve beer_song algorithm problem

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -4,7 +4,20 @@ defmodule BeerSong do
   """
   @spec verse(integer) :: String.t()
   def verse(number) do
-    # Your implementation here...
+    cond do
+      number > 2 ->
+        prep_verse(number) <> "#{number - 1} bottles of beer on the wall.\n"
+      number > 1 ->
+        prep_verse(number) <> "#{number - 1} bottle of beer on the wall.\n"
+      number > 0 ->
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      true ->
+        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    end
+  end
+
+  defp prep_verse(number) do
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, "
   end
 
   @doc """
@@ -12,6 +25,21 @@ defmodule BeerSong do
   """
   @spec lyrics(Range.t()) :: String.t()
   def lyrics(range) do
-    # Your implementation here...
+    verseRec(Enum.to_list range)
+  end
+
+  def lyrics() do
+    lyrics(99..0)
+  end
+
+  defp verseRec(list, res \\ "")
+  
+  defp verseRec([h | t], res) do
+    res = res <> "\n" <> verse(h)
+    verseRec(t, res)
+  end
+  
+  defp verseRec([], res) do
+    res |> String.trim_leading
   end
 end
